254dd28e57da92a83cdc25fa711b45589839ff05
Q-Mart <qumarthjash@gmail.com>
2017-10-02 13:23:13 +0100

    Added states and pango markdown

--- casset.py
+++ casset.py
@@ -1,22 +1,26 @@
+#!/usr/bin/python
+
 import csv
 import yaml
+import os
 from api import CoinBaseAPI
 
+dirPath = os.path.dirname(os.path.realpath(__file__))+'/'
 api_cfg = {}
-with open('api_config.yaml') as f:
+with open(dirPath+'api_config.yaml') as f:
     api_cfg = yaml.load(f)
 
 api = CoinBaseAPI(api_cfg['key'], api_cfg['secret'], api_cfg['version'])
 
 
-def prettyFmt(BTC, LTC, ETH):
+def prettyFmt(BTC, ETH, LTC):
     colorise = lambda x: '<span foreground="#00FF00">{0}</span>'.format(x)
     colorisedCoins = list(map(colorise, [BTC, ETH, LTC]))
     return ':{0} Ξ:{1} Ł:{2}'.format(*colorisedCoins)
 
 def getROIs():
     initialInvestment = {}
-    with open('transaction_log.csv') as csvfile:
+    with open(dirPath+'transaction_log.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             currency = row['currency']
@@ -52,5 +56,13 @@ def getSpotPrices():
     ETHSpot = api.getSpotPrice('ETH')['amount']
     return prettyFmt(BTCSpot, ETHSpot, LTCSpot)
 
-print(getSpotPrices())
-print(getROIs())
+with open(dirPath+'.STATE') as statefile:
+    state = statefile.read()
+
+with open(dirPath+'.STATE', 'w') as statefile:
+    if state == 'ROI':
+        print(getROIs())
+        statefile.write('PRICE')
+    else:
+        print(getSpotPrices())
+        statefile.write('ROI')
